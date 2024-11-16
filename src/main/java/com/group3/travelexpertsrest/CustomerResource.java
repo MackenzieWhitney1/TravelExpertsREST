10550c47283ee890bdd4efb87f09c52a65160a30
ErinBough <erinb7@gmail.com>
2024-11-16 05:33:40 -0700

    Added CustomerResource and CustomersDB

---
+++ src/main/java/com/group3/travelexpertsrest/CustomerResource.java
@@ -0,0 +1,57 @@
+package com.group3.travelexpertsrest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import data.CustomersDB;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import model.Customer;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+@Path("/customers")
+public class CustomerResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCustomers() {
+        return CustomersDB.getCustomers();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("getcustomer/{ customerId }")
+    public String getCustomerById(@PathParam("customerId") int customerId) {
+        return CustomersDB.getCustomerById(customerId);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("addcustomer")
+    public String addCustomer(String jsonString) {
+        return CustomersDB.addCustomer(jsonString);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("updatecustomer")
+    public String putPackage(String jsonString) {
+        return CustomersDB.updateCustomer(jsonString);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("deletecustomer/{customerId}")
+    public String deletePackage(@PathParam("customerId") int customerId) {
+        return CustomersDB.deleteCustomer(customerId);
+    }
+
+}
