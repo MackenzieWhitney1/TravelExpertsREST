911ec3b1c9bd6447ecb5dff74876abd0a3b38ef3
Mackenzie Whitney <mackenziewhtn@gmail.com>
2024-11-20 18:25:30 -0700

    Update the CreditCard resource and DB to use DTOs.
    The primary reason for this was that the previous sent json was not formatted in a way that the Android client could convert from

--- src/main/java/data/CreditcardDB.java
+++ src/main/java/data/CreditcardDB.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Query;
 import model.Creditcard;
+import model.CreditcardDTO;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -15,15 +16,12 @@ import static com.group3.travelexpertsrest.TravelExpertsDB.createFactory;
 public class CreditcardDB {
     private static String jsonResult = null;
     private static final JsonObject result = new JsonObject();
-    public static String getRewards() {
+    public static String getAllCreditcards() {
         EntityManager em;
         try (EntityManagerFactory emf = createFactory()) {
             em = emf.createEntityManager();
-            Query q = em.createQuery("select c.id, c.cCName, c.cCExpiry, c.cCNumber, c.customer.id from Creditcard c");
-            List<Creditcard> creditcardlist = q.getResultList();
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<Creditcard>>() {}.getType();
-            jsonResult = gson.toJson(creditcardlist, listType);
+            Query q = em.createQuery("select c from Creditcard c");
+            getCreditCardDTOsFromQuery(q);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -34,12 +32,27 @@ public class CreditcardDB {
         EntityManager em;
         try (EntityManagerFactory emf = createFactory()) {
             em = emf.createEntityManager();
-            Query q = em.createQuery("select c.id, c.cCName, c.cCNumber, c.cCExpiry from Creditcard c where c.customer.id = :customerId");
+            Query q = em.createQuery("select c from Creditcard c where c.customer.id = :customerId");
             q.setParameter("customerId", customerId);
-            List customerCreditcardList = q.getResultList();
-            Gson gson = new Gson();
-            jsonResult = gson.toJson(customerCreditcardList);
+            getCreditCardDTOsFromQuery(q);
         }
         return jsonResult;
+    }
+
+    private static void getCreditCardDTOsFromQuery(Query q) {
+        List<Creditcard> creditcardlist = q.getResultList();
+
+        List<CreditcardDTO> listCreditcardDTO = creditcardlist.stream().map(
+                cc -> new CreditcardDTO(
+                        cc.getId(),
+                        cc.getCustomer().getId(),
+                        cc.getCCNumber(),
+                        cc.getCCExpiry(),
+                        cc.getCCName()
+                )).toList();
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<CreditcardDTO>>() {}.getType();
+        jsonResult = gson.toJson(listCreditcardDTO, listType);
     }
 }
